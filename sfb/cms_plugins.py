6a21c7672ed90b41e59830007db24b98740e3e91
faebser <fabian.frei@esf-frei.ch>
2014-05-23 16:20:28 +0200

    added templates, reworked models

--- sfb/cms_plugins.py
+++ sfb/cms_plugins.py
@@ -1,8 +1,8 @@
+# -*- coding: utf-8 -*-
 __author__ = 'faebser'
 
 from cms.plugin_base import CMSPluginBase
 from cms.plugin_pool import plugin_pool
-from cms.models.pluginmodel import CMSPlugin
 from django.utils.translation import ugettext_lazy as _
 from djangocms_text_ckeditor.cms_plugins import TextPlugin
 from models import *
@@ -22,11 +22,70 @@ class DefaultPlugin(TextPlugin):
     render_template = path.join(ps.templatePath, "default_plugin.html")
 
 
-class ArticlePageIntro(TextPlugin):
+class ArticlePageIntro(CMSPluginBase):
     name = u'Artikel Intro'
     module = ps.module
     model = SfbArticlePageHeader
     render_template = path.join(ps.templatePath, "default_plugin.html")
 
+
+class ArticlePageTeaser(CMSPluginBase):
+    name = u"Artikel Teaser"
+    module = ps.module
+    model = SfbArticleTeaser
+    render_template = path.join(ps.templatePath, "default_plugin.html")
+
+
+class NewsPageIntro(CMSPluginBase):
+    name = u"News Intro"
+    module = ps.module
+    model = SfbNewsPageHeader
+    render_template = path.join(ps.templatePath, "default_plugin.html")
+
+
+class NewsPageTeaser(CMSPluginBase):
+    name = u'News Teaser'
+    module = ps.module
+    model = SfbNewsTeaser
+    render_template = path.join(ps.templatePath, "default_plugin.html")
+
+
+class PdfBox(CMSPluginBase):
+    name = u'PDF-Box'
+    module = ps.module
+    model = SfbPDFBox
+    render_template = path.join(ps.templatePath, "default_plugin.html")
+    allow_children = True
+    child_classes = ["PdfLink"]
+
+
+class PdfLink(CMSPluginBase):
+    name = u'PDF'
+    module = ps.module
+    model = SfbPDFLink
+    render_template = path.join(ps.templatePath, "default_plugin.html")
+
+
+class LinkBox(CMSPluginBase):
+    name = u'Link-Box'
+    module = ps.module
+    model = SfbLinkBox
+    render_template = path.join(ps.templatePath, "default_plugin.html")
+    allow_children = True
+    child_classes = ["InternalLink", "ExternalLink"]
+
+
+class InternalLink(CMSPluginBase):
+    name = u'Interner Link'
+    module = ps.module
+    model = SfbInternalLink
+    render_template = path.join(ps.templatePath, "default_plugin.html")
+
+
+class ExternalLink(CMSPluginBase):
+    name = u'Externer Link'
+    module = ps.module
+    model = SfbExternalLink
+    render_template = path.join(ps.templatePath, "default_plugin.html")
 
 plugin_pool.register_plugin(DefaultPlugin)
--- sfb/models.py
+++ sfb/models.py
@@ -1,9 +1,11 @@
+# -*- coding: utf-8 -*-
 __author__ = 'faebser'
 
 from django.db import models
 from cms.models import Page
 from cms.models.pluginmodel import CMSPlugin  # normal cms-plugin
 from djangocms_text_ckeditor.models import AbstractText  # text plugin
+from djangocms_text_ckeditor.fields import HTMLField  # html field
 from django.utils.translation import ugettext_lazy as _
 import datetime
 
@@ -12,27 +14,27 @@ class SfbDefaultText(AbstractText):
     title = models.CharField(max_length=256)
 
 
-class SfbArticlePageHeader(AbstractText):
+class SfbArticlePageHeader(CMSPlugin):
     title = models.CharField(max_length=256, verbose_name=u"Titel")
     author = models.CharField(max_length=256, verbose_name=u"Autor")
     image = models.ImageField(verbose_name=u"Bild", upload_to=CMSPlugin.get_media_path, default="<empty>")
-    publicationDate = models.DateTimeField(verbose_name=u"Datum/Zeit", default=datetime.now)
-    body = models.TextField(_("body"), verbose_name=u"Abriss")
+    publicationDate = models.DateTimeField(verbose_name=u"Datum/Zeit", default=datetime.datetime.now())
+    body = HTMLField(verbose_name=u"Abriss")
 
     def __unicode__(self):
-        return "Artikel: " + self.title[0:20] + "... von " + self.author + " / " + str(self.publicationDate.strftime("%Y%m%d%H%M%S"))
+        return "Artikel: " + self.title[0:20] + "... von " + self.author + " / " + str(self.publicationDate.strftime("%H:%M:%S %m/%d/%Y"))
 
     class Meta:
         verbose_name = "Artikel Intro"
 
 
-class SfbArticleTeaser(AbstractText):
+class SfbArticleTeaser(CMSPlugin):
     teaserFor = models.ForeignKey(SfbArticlePageHeader, verbose_name=u"Verweis auf")
     titleOverwrite = models.CharField(max_length=256, verbose_name=u"Titel")
     authorOverwrite = models.CharField(max_length=256, verbose_name=u"Autor")
     imageOverwrite = models.ImageField(verbose_name=u"Bild", upload_to=CMSPlugin.get_media_path, default="<empty>")
-    publicationDateOverwrite = models.DateTimeField(verbose_name=u"Datum/Zeit", default=datetime.now)
-    bodyOverwrite = models.TextField(_("body"), verbose_name=u"Abriss")
+    publicationDateOverwrite = models.DateTimeField(verbose_name=u"Datum/Zeit", default=datetime.datetime.now())
+    bodyOverwrite = models.TextField(verbose_name=u"Abriss")
 
     class Meta:
         verbose_name = "Verweis auf Artikel"
@@ -50,8 +52,20 @@ class SfbNewsTeaser(SfbArticleTeaser):
         verbose_name = "Verweis auf eine News"
 
 
+class SfbPDFBox(CMSPlugin):
+
+    def __unicode__(self):
+        return u'PDF-Box'
+
+
+class SfbLinkBox(CMSPlugin):
+
+    def __unicode__(self):
+        return u'Link-Box'
+
+
 class SfbPDFLink(CMSPlugin):
-    file = models.FileField(verbose_name=u"PDF-Datei", upload_to=CMSPlugin.get_media_path, default="<empty>")
+    pdfFile = models.FileField(verbose_name=u"PDF-Datei", upload_to=CMSPlugin.get_media_path, default="<empty>")
     text = models.CharField(max_length=256, verbose_name=u"Text für Link")
 
     def __unicode__(self):
@@ -60,7 +74,11 @@ class SfbPDFLink(CMSPlugin):
 
 class SfbInternalLink(CMSPlugin):
     name = models.CharField(max_length=256, verbose_name=u"Text für Link")
-    link = models.ForeignKey(Page, verbose_name=u"interne Seite")
+    internalHref = models.ForeignKey(Page, verbose_name=u"interne Seite")
+    target = models.CharField(verbose_name=u'Öffnen in', blank=True, default=("_blank", u"gleichem Fenster"), max_length=100, choices=((
+        ("", _("gleichem Fenster")),
+        ("_blank", _("neuem Fenster")),
+    )))
 
     def __unicode__(self):
         return self.name
@@ -68,7 +86,11 @@ class SfbInternalLink(CMSPlugin):
 
 class SfbExternalLink(CMSPlugin):
     name = models.CharField(max_length=256, verbose_name=u"Text für Link")
-    link = models.URLField(verbose_name=u"externer Link")
+    externalHref = models.URLField(verbose_name=u"externer Link")
+    target = models.CharField(verbose_name=u'Öffnen in', blank=True, default=("_blank", u"gleichem Fenster"), max_length=100, choices=((
+        ("", _("gleichem Fenster")),
+        ("_blank", _("neuem Fenster")),
+    )))
 
     def __unicode__(self):
         return self.name
