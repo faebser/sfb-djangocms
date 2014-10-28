f8f4c2a284e666a89a6e32dfb374cf9421b9922c
Fabian Frei <fabian.frei@esf-frei.ch>
2014-10-28 02:42:55 +0100

    added new app for the paper, fixed some css

--- sfb/admin.py
+++ sfb/admin.py
@@ -2,6 +2,13 @@ __author__ = 'faebser'
 
 from django.contrib import admin
 from sfb_shop import models
+from sfb_paper import models as paper
 
 admin.site.register(models.PriceModelItem)
 admin.site.register(models.PriceModel)
+
+admin.site.register(paper.Tag)
+admin.site.register(paper.Author)
+admin.site.register(paper.Article)
+admin.site.register(paper.Download)
+admin.site.register(paper.Issue)
---
+++ sfb_paper/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from cms.models.pluginmodel import CMSPlugin  # normal cms-plugin
+from djangocms_text_ckeditor.fields import HTMLField  # html field
+
+# Create your models here.
+
+
+def get_path(self, filename):
+        return 'bla'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=256, verbose_name=u'Name')
+
+
+class Author(models.Model):
+    name = models.CharField(max_length=256, verbose_name=u'Name')
+
+
+class Article(models.Model):
+    authors = models.ManyToManyField(Author, verbose_name=u'Autoren')
+    issue = models.ForeignKey(Issue, verbose_name=u'Ausgabe')
+    download = models.ForeignKey(Download, verbose_name=u'Download')
+    tags = models.ManyToManyField(Tag, verbose_name=u'Schlagworte')
+
+
+class Download(models.Model):
+    pdf = models.FileField(verbose_name=u'Datei', upload_to=get_path)
+
+
+class Issue(CMSPlugin):
+    name = models.CharField(max_length=256, verbose_name=u'Name')
+    year = models.IntegerField(verbose_name=u'Jahr')
+    month = models.IntegerField(verbose_name=u'Monat')
+    download = models.ForeignKey(Download, verbose_name=u'Download')
+
