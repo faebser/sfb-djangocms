613b4e42c6ba2e52d88e5faf2060837fddf95bf5
Fabian Frei <fabian.frei@esf-frei.ch>
2014-11-18 02:49:06 +0100

    removed django-select2 urls

--- sfb/urls.py
+++ sfb/urls.py
@@ -10,7 +10,6 @@ admin.autodiscover()
 urlpatterns = i18n_patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^sitemap\.xml$', 'django.contrib.sitemaps.views.sitemap', {'sitemaps': {'cmspages': CMSSitemap}}),
-    url(r'^select2/', include('django_select2.urls')),
     url(r'^', include('cms.urls')),
 )
 
